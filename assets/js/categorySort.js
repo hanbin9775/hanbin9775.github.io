97897330b1d8238bda32c4c0146a4ab44ec157bb
Hanbin <dnffkrktyd@gmail.com>
2020-05-04 22:32:06 +0900

    addEventListenr to sidebarmenu buttons

--- assets/js/categorySort.js
+++ assets/js/categorySort.js
@@ -1,6 +1,6 @@
 var buttons = document.querySelectorAll(".childMenu li");
 
-console.log(buttons);
+
 
 var parent = document.querySelector("#postList ul");
 var childs = document.querySelectorAll(".postContent");
@@ -8,30 +8,18 @@ var childs = document.querySelectorAll(".postContent");
 console.log(parent);
 console.log(childs);
 
-buttons[0].addEventListener("click", ()=>{
-    childs.forEach(e =>{
-        console.log(e.id);
-        e.remove();
+buttons.forEach(e=>{
+    console.log(e);
+    e.addEventListener("click", ()=>{
+        //한번 비워주고
+        childs.forEach(e =>{
+            e.remove();
+        });
+        //카테고리에 해당하는 포스팅만 표시
+        childs.forEach(c=>{
+            if(c.id === e.id ){
+                parent.appendChild(c);
+            }
+        });
     });
-    console.log("deleted");
-    childs.forEach(e=>{
-        if(e.id === "front"){
-            parent.appendChild(e);
-        }
-    });
-    console.log("appended");
-});
-
-buttons[2].addEventListener("click", ()=>{
-    childs.forEach(e =>{
-        console.log(e.id);
-        e.remove();
-    });
-    console.log("deleted");
-    childs.forEach(e=>{
-        if(e.id === "algorithm"){
-            parent.appendChild(e);
-        }
-    });
-    console.log("appended");
-});
+})
