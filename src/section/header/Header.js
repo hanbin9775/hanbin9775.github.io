7b4442bf37a1ee8ebe520fd4010fedd981909754
hanbin9775 <dnffkrktyd@gmail.com>
2021-06-11 00:14:19 +0900

    phone number 숨김처리

--- src/section/header/Header.js
+++ src/section/header/Header.js
@@ -15,7 +15,7 @@ export default function Header({ $app }) {
   const headerRight = new HeaderRight({
     $app: $header,
     birthDay: "1995.09.15",
-    phoneNumber: "010-3945-9775",
+    phoneNumber: "010-XXXX-XXXX",
     email: "dnffkrktyd@gmail.com",
     githubUrl: "https://github.com/hanbin9775",
   });
