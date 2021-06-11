76579cacb0cb4eeda893a53fc2eabc0ad62472e2
hanbin9775 <dnffkrktyd@gmail.com>
2021-06-11 21:26:52 +0900

    Header 정보 App에서 넘겨주게 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Header from "./section/header/Header.js";
+// import EducationAndCarrer from "./section/body/EducationAndCarrer.js";
 
 export default function App($app) {
   const $content = document.createElement("div");
   $content.className = "content";
   $app.appendChild($content);
 
+  //Header Section
   const header = new Header({
     $app: $content,
+    koreanName: "김한빈",
+    englishName: "Hanbin Kim",
+    job: "Frontend Developer",
+    birthDay: "1995.09.15",
+    phoneNumber: "010-XXXX-XXXX",
+    email: "dnffkrktyd@gmail.com",
+    githubUrl: "https://github.com/hanbin9775",
   });
 
-  console.log(header);
+  //Education and Carrer Section
+  // const educationAndCarrer = new EducationAndCarrer({ $app: $content });
 }
--- src/section/header/Header.js
+++ src/section/header/Header.js
@@ -1,22 +1,31 @@
 import HeaderLeft from "./HeaderLeft.js";
 import HeaderRight from "./HeaderRight.js";
 
-export default function Header({ $app }) {
+export default function Header({
+  $app,
+  koreanName,
+  englishName,
+  job,
+  birthDay,
+  phoneNumber,
+  email,
+  githubUrl,
+}) {
   const $header = document.createElement("header");
   $header.className = "header";
   $app.appendChild($header);
 
   const headerLeft = new HeaderLeft({
     $app: $header,
-    koreanName: "김한빈",
-    englishName: "Hanbin Kim",
-    job: "Frontend Developer",
+    koreanName: koreanName,
+    englishName: englishName,
+    job: job,
   });
   const headerRight = new HeaderRight({
     $app: $header,
-    birthDay: "1995.09.15",
-    phoneNumber: "010-XXXX-XXXX",
-    email: "dnffkrktyd@gmail.com",
-    githubUrl: "https://github.com/hanbin9775",
+    birthDay: birthDay,
+    phoneNumber: phoneNumber,
+    email: email,
+    githubUrl: githubUrl,
   });
 }
