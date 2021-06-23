53ed30acf1747bb71bc1c0c3fa6d238c0ac0804d
hanbin9775 <dnffkrktyd@gmail.com>
2021-06-23 23:47:28 +0900

    [#1] plugin added for sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,11 +7,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: ["MiniCssExtractPlugin.loader", "style-loader", "css-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
